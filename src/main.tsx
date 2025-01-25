df730c48b108197dedc60d722e192f4ad14ea173
kavin bharathi <kavinbharathi.mahendran@gmail.com>
2025-01-25 10:19:48 +0530

    feat : add posthog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
-import App from './App.tsx'
+import App from './App.tsx';
+import { PostHogProvider } from 'posthog-js/react';
+
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com', // Default PostHog API host
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider 
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY || 'YOUR_API_KEY'} 
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
-)
+);
